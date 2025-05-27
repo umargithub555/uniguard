85b6f8c1c170a83db2374d137a91da6067d8a87a
Shayan umar <shayanumar5555@gmail.com>
2025-05-27 13:40:29 +0500

    Resolved merge conflicts

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -23,17 +23,6 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
 
 @router.post("/login")
 def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-<<<<<<< HEAD
-    user = db.query(User).filter(User.email == form_data.username).first()
-    if not user or not verify_password(form_data.password, user.password_hash):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    token = create_jwt_token({"sub": user.email})
-    return {"access_token": token, "token_type": "bearer", "user_id": user.id, "name": user.name, "role": user.role}
-=======
     print(f"Attempting login with username: {form_data.username}")
     user = db.query(User).filter(User.email == form_data.username).first()
     
@@ -72,7 +61,6 @@ def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = D
             headers={"WWW-Authenticate": "Bearer"},
         )
 
->>>>>>> 9d5ba82d98f1c663ea9ad1fb235f1d31d64cbbcd
 
 @router.get("/me", response_model=UserResponse)
 def get_user_me(current_user: User = Depends(get_current_user)):
